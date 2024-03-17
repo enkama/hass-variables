7e57983524c088a806ae710c02dcf0c5762ddb0c
Snuffy2 <Snuffy2@users.noreply.github.com>
2024-03-16 21:09:05 -0400

    Fix Hassfest CONFIG_SCHEMA Warning

--- custom_components/variable/__init__.py
+++ custom_components/variable/__init__.py
@@ -63,6 +63,7 @@ SERVICE_SET_ENTITY_LEGACY_SCHEMA = vol.Schema(
         ): cv.boolean,
     }
 )
+CONFIG_SCHEMA = cv.platform_only_config_schema(DOMAIN)
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType):
